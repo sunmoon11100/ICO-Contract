2be537bc4df4a15e15ad286614e4b3e2fa12d0e6
Victor Han <sunmoon11@yandex.ru>
2021-07-18 01:48:49 +0300

    Update YRCH_ICO.sol

--- YRCH_ICO.sol
+++ YRCH_ICO.sol
@@ -28,11 +28,51 @@ contract YRCH_ICO is YRCHToken{
         _;
     }
 
-    event
+    event Invest(address _investor, uint _value, uint _tokens);
 
     constructor(address _admin, address payable _deposit){
+        admin = _admin;
+        deposit = _deposit;
+        price = 0.0001 ether;
+        hardCap = 1000 ether;
+        raisedAmount = 0;
+        dateStartICO = block.timestamp;
+        dateEndICO = dateStartICO + 1 days;
+        dateUnlock = dateEndICO + 180 days;
+        maxInv = 10 ether;
+        minInv = 0.001 ether;
+        state = State.beforeStart;
+    }
+
+    function invest() payable public notAdmin returns(address){
 
     }
+
+    receive() payable external{
+        invest();
+    }
+
+    function getStatus() public view returns(string memory){
+        string memory result;
+        if(state == State.beforeStart){
+            result = "Not Started Yet";
+        }else if(state == State.running){
+            result = "Running";
+        }else if(state == State.ended){
+            result = "Ended";
+        }else
+            result = "halted";
+
+        return result;
+    }
+
+    function burn() public notAdmin returns(bool){
+
+    }
+
+    
+
+
 
     
 
